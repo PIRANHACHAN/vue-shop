dccff79cd7732459a8357416803057b1dfeb75b2
PIRANHA <kobeel@163.com>
2020-08-26 13:27:41 +0800

    基本完成侧边栏

--- src/components/Aside.vue
+++ src/components/Aside.vue
@@ -5,6 +5,7 @@
     active-text-color="#4b9bff"
     unique-opened
     router
+    :default-active="activePath"
   >
     <el-submenu :index="item.id + ''" v-for="item in menuList" :key="item.id">
       <template slot="title">
@@ -12,9 +13,10 @@
         <span>{{ item.authName }}</span>
       </template>
       <el-menu-item
-        :index="'/' + childItem.path + ''"
+        :index="'/' + childItem.path"
         v-for="childItem in item.children"
         :key="childItem.id"
+        @click="saveNavState('/' + childItem.path)"
       >
         <template slot="title">
           <i class="el-icon-menu"></i>
@@ -38,17 +40,23 @@ export default {
         '102': 'el-icon-s-order',
         '145': 'el-icon-s-data',
       },
+      activePath: '',
     }
   },
   created() {
     this.getMenuList()
+    this.activePath = window.sessionStorage.getItem('activePath')
   },
   methods: {
     async getMenuList() {
-      const { data: res } = await this.$http.get('menus')
-      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
-      this.menuList = res.data
-      console.log(this.menuList)
+      const { data: result } = await this.$http.get('menus')
+      if (result.meta.status !== 200)
+        return this.$message.error(result.meta.msg)
+      this.menuList = result.data
+    },
+    saveNavState(activePath) {
+      window.sessionStorage.setItem('activePath', activePath)
+      this.activePath = activePath
     },
   },
 }
