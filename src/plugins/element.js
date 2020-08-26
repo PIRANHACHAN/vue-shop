d4bbd64d38973af41e5c6c18ebbaa1bc5deb8905
PIRANHA <kobeel@163.com>
2020-08-26 13:30:57 +0800

    用户列表开始搭建页面

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -12,6 +12,11 @@ import {
   Menu,
   Submenu,
   MenuItem,
+  Breadcrumb,
+  BreadcrumbItem,
+  Card,
+  Row,
+  Col,
 } from 'element-ui'
 
 Vue.use(Button)
@@ -25,5 +30,10 @@ Vue.use(Main)
 Vue.use(Menu)
 Vue.use(Submenu)
 Vue.use(MenuItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
 
 Vue.prototype.$message = Message
