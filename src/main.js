83246eabb9c19b27c67ff994d6f1121df12386d3
PIRANHA <kobeel@163.com>
2020-09-07 13:19:22 +0800

    配置页面顶端进度条方法

--- src/main.js
+++ src/main.js
@@ -8,22 +8,33 @@ import VueQuillEditor from 'vue-quill-editor'
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
+import Nprogress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 import axios from 'axios'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
-// 请求拦截
 axios.interceptors.request.use((config) => {
+  // 页面顶端进度条
+  Nprogress.start()
+
   config.headers.Authorization = window.sessionStorage.getItem('token')
   return config
 })
-
+axios.interceptors.response.use((config) => {
+  Nprogress.done()
+  return config
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
+// 第三方树状表格
 Vue.component('tree-table', TreeTable)
+
+// 富文本编辑器
 Vue.use(VueQuillEditor)
 
+// 日期格式化
 Vue.filter('dateFormat', function(originVal) {
   const dt = new Date(originVal)
 
