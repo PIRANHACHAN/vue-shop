390008f13e6d902f7aafcea70ef08c4d782e4625
PIRANHA <kobeel@163.com>
2020-09-07 17:05:43 +0800

    打包构建完成

---
+++ src/main-prod.js
@@ -0,0 +1,50 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import './assets/css/global.css'
+import TreeTable from 'vue-table-with-tree-grid'
+import VueQuillEditor from 'vue-quill-editor'
+import Nprogress from 'nprogress'
+
+import axios from 'axios'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.interceptors.request.use((config) => {
+  // 页面顶端进度条
+  Nprogress.start()
+
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+axios.interceptors.response.use((config) => {
+  Nprogress.done()
+  return config
+})
+Vue.prototype.$http = axios
+
+Vue.config.productionTip = false
+
+// 第三方树状表格
+Vue.component('tree-table', TreeTable)
+
+// 富文本编辑器
+Vue.use(VueQuillEditor)
+
+// 日期格式化
+Vue.filter('dateFormat', function(originVal) {
+  const dt = new Date(originVal)
+
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
+
+new Vue({
+  router,
+  render: (h) => h(App),
+}).$mount('#app')
