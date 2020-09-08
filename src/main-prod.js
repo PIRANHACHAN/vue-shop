c3136dc04d0df648e0c0fe42a97fe2027d18ec0a
PIRANHA <kobeel@163.com>
2020-09-08 11:40:16 +0800

    取消element的cdn加速，部署线上后出错

--- src/main-prod.js
+++ src/main-prod.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './plugins/element.js'
 import './assets/css/global.css'
 import TreeTable from 'vue-table-with-tree-grid'
 import VueQuillEditor from 'vue-quill-editor'
