ef1c160da06499f16033434f18723d3acc6e7895
PIRANHA <kobeel@163.com>
2020-09-08 15:34:08 +0800

    封装axios

--- src/main-dev.js
+++ src/main-dev.js
@@ -1,51 +1,31 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import './plugins/element.js'
-import './assets/css/global.css'
+import App from '@/App.vue'
+import router from '@/router'
+import '@/plugins/element.js'
+import '@/assets/css/global.css'
+// 树状表格
 import TreeTable from 'vue-table-with-tree-grid'
+// 富文本编辑器
 import VueQuillEditor from 'vue-quill-editor'
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
-import Nprogress from 'nprogress'
-import 'nprogress/nprogress.css'
-
-import axios from 'axios'
-axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
-axios.interceptors.request.use((config) => {
-  // 页面顶端进度条
-  Nprogress.start()
-
-  config.headers.Authorization = window.sessionStorage.getItem('token')
-  return config
-})
-axios.interceptors.response.use((config) => {
-  Nprogress.done()
-  return config
-})
-Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
-// 第三方树状表格
 Vue.component('tree-table', TreeTable)
 
-// 富文本编辑器
 Vue.use(VueQuillEditor)
 
-// 日期格式化
+// 日期过滤器
 Vue.filter('dateFormat', function(originVal) {
   const dt = new Date(originVal)
-
   const y = dt.getFullYear()
   const m = (dt.getMonth() + 1 + '').padStart(2, '0')
   const d = (dt.getDate() + '').padStart(2, '0')
-
   const hh = (dt.getHours() + '').padStart(2, '0')
   const mm = (dt.getMinutes() + '').padStart(2, '0')
   const ss = (dt.getSeconds() + '').padStart(2, '0')
-
   return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
 })
 
---
+++ src/utils/request.js
@@ -0,0 +1,51 @@
+import axios from 'axios'
+import { Message } from 'element-ui'
+import Nprogress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+// 创建一个axios的对象
+const $http = axios.create({
+  // baseURL: 'http://api.piranhachan.top/api/private/v1/',
+  baseURL: 'http://127.0.0.1:8888/api/private/v1/',
+  timeout: 8000,
+})
+
+// request拦截器
+$http.interceptors.request.use(
+  (config) => {
+    // 页面顶端进度条
+    Nprogress.start()
+
+    config.headers['Authorization'] = window.sessionStorage.getItem('token')
+    return config
+  },
+  (error) => {
+    Message.error('请求超时，请重试！')
+    return Promise.reject(error)
+  }
+)
+
+//response拦截器
+$http.interceptors.response.use(
+  (response) => {
+    Nprogress.done()
+
+    const res = response.data
+    if (res.meta.status !== 200) {
+      if (res.meta.status !== 201) {
+        Message({
+          type: 'error',
+          message: res.meta.msg,
+        })
+      }
+      return
+    }
+    return res
+  },
+  (error) => {
+    Message.error('请求失败，请稍后重试！')
+    return Promise.reject(error)
+  }
+)
+
+export default $http
