c9332096bd52144ed7cdf2b0e69a498fa8700d0d
PIRANHA <kobeel@163.com>
2020-09-09 11:20:35 +0800

    修改el-table的margin值为0

--- src/components/power/Rights.vue
+++ src/components/power/Rights.vue
@@ -1,6 +1,6 @@
 <template>
   <el-card>
-    <el-table :data="rightsList" border stripe>
+    <el-table :data="rightsList" border stripe style="margin-top: 0;">
       <el-table-column label="#" type="index"></el-table-column>
       <el-table-column label="权限名称" prop="authName"></el-table-column>
       <el-table-column label="路径" prop="path"></el-table-column>
