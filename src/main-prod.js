ddd08df3b1c9404997473a3e868ece1504a42f55
PIRANHA <kobeel@163.com>
2020-09-09 15:52:17 +0800

    update日期过滤器

--- src/main-prod.js
+++ src/main-prod.js
@@ -19,7 +19,7 @@ Vue.use(VueQuillEditor)
 
 // 日期过滤器
 Vue.filter('dateFormat', function(originVal) {
-  const dt = new Date(originVal)
+  const dt = new Date(originVal * 1000)
   const y = dt.getFullYear()
   const m = (dt.getMonth() + 1 + '').padStart(2, '0')
   const d = (dt.getDate() + '').padStart(2, '0')
